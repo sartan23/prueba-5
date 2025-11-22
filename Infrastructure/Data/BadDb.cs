92a13b41f51f9aea5daace7d3f9c3555c4c23ef9
sartan23 <42255870+sartan23@users.noreply.github.com>
2025-11-22 11:54:21 -0500

    primera carga

---
+++ Infrastructure/Data/BadDb.cs
@@ -0,0 +1,25 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Infrastructure.Data;
+
+public static class BadDb
+{    
+    public static string ConnectionString { get; set; }
+
+    public static int ExecuteNonQueryUnsafe(string sql)
+    {
+        var conn = new SqlConnection(ConnectionString);
+        var cmd = new SqlCommand(sql, conn);
+        conn.Open();
+        return cmd.ExecuteNonQuery();
+    }
+
+    public static IDataReader ExecuteReaderUnsafe(string sql)
+    {
+        var conn = new SqlConnection(ConnectionString);
+        var cmd = new SqlCommand(sql, conn);
+        conn.Open();
+        return cmd.ExecuteReader();
+    }
+}
